7cdc2a9ee8098efef902235436151b7124f2a5df
Joey <haskaalo@gmail.com>
2021-06-27 21:23:48 -0400

    Changed random string generation for security purposes

--- utils/string.go
+++ utils/string.go
@@ -1,21 +1,24 @@
 package utils
 
 import (
-	"math/rand"
-	"time"
+	"crypto/rand"
+	"math/big"
 )
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
 // RandString return random string with specified length
 func RandString(n int) string {
 	b := make([]rune, n)
+
 	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+		randomLetterPosition, err := rand.Int(rand.Reader, big.NewInt(int64(len(letterRunes))))
+		if err != nil {
+			panic(err)
+		}
+
+		b[i] = letterRunes[randomLetterPosition.Int64()]
 	}
+
 	return string(b)
 }
